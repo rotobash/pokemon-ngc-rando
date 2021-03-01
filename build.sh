baf3979cca5d338cfa9f84fc9401b65193498e6b
Michael Petrella <rotobash@gmail.com>
2021-03-01 06:54:35 -0500

    add build script

---
+++ build.sh
@@ -0,0 +1,14 @@
+if [[ -z $1 ]]
+then
+  PATH='.'
+else
+  PATH="$1"
+fi
+
+echo $PATH
+
+mkdir -p "$PATH/ngc-randomizer"
+mkdir -p "$PATH/ngc-randomizer-sc"
+
+dotnet publish --configuration 'Release' --self-contained true -o "$1/ngc-randomizer-sc"
+dotnet publish --configuration 'Release' --self-contained false -o "$1/ngc-randomizer"
