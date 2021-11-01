e896ef55e4740304cdff4beb5cc8be71a8231104
Mike Petrella <rotobash@gmail.com>
2021-10-31 23:03:58 -0400

    fix build script

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 if [[ -z $1 ]]
 then
   PATH='.'
@@ -10,5 +12,5 @@ echo $PATH
 mkdir -p "$PATH/ngc-randomizer"
 mkdir -p "$PATH/ngc-randomizer-sc"
 
-dotnet publish --configuration 'Release' --self-contained true -o "$1/ngc-randomizer-sc"
+dotnet publish --configuration 'Release' --self-contained true --runtime win-x64 -o "$1/ngc-randomizer-sc"
 dotnet publish --configuration 'Release' --self-contained false -o "$1/ngc-randomizer"
