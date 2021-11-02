d856227bb25c92865af78e3ca60c10fc913f63f5
Michael Petrella <rotobash@gmail.com>
2021-11-02 08:03:55 -0400

    one last fix to the build script

--- build.sh
+++ build.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-if [[ -z $1 ]]
-then
-  OUT='.'
-else
-  OUT="$1"
-fi
+mkdir -p "ngc-randomizer"
+mkdir -p "ngc-randomizer-sc"
 
-echo $OUT
-
-mkdir -p "$OUT/ngc-randomizer"
-mkdir -p "$OUT/ngc-randomizer-sc"
-
-dotnet publish --configuration 'Release' --self-contained true --runtime win-x64 -o "$1/ngc-randomizer-sc"
-dotnet publish --configuration 'Release' --self-contained false -o "$1/ngc-randomizer"
+dotnet publish --configuration 'Release' --self-contained true --runtime win-x64 -o "ngc-randomizer-sc"
+dotnet publish --configuration 'Release' --self-contained false -o "ngc-randomizer"
