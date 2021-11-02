a0ab0bf6755794834f1c203b928c18787c8e4fb1
Michael Petrella <rotobash@gmail.com>
2021-11-02 08:01:49 -0400

    fix build script while I'm at because why not

--- build.sh
+++ build.sh
@@ -2,15 +2,15 @@
 
 if [[ -z $1 ]]
 then
-  PATH='.'
+  OUT='.'
 else
-  PATH="$1"
+  OUT="$1"
 fi
 
-echo $PATH
+echo $OUT
 
-mkdir -p "$PATH/ngc-randomizer"
-mkdir -p "$PATH/ngc-randomizer-sc"
+mkdir -p "$OUT/ngc-randomizer"
+mkdir -p "$OUT/ngc-randomizer-sc"
 
 dotnet publish --configuration 'Release' --self-contained true --runtime win-x64 -o "$1/ngc-randomizer-sc"
 dotnet publish --configuration 'Release' --self-contained false -o "$1/ngc-randomizer"
