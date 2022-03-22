5400b0b074d30c7a7fc3a4bce9e036e378f42f6e
Mike Petrella <rotobash@gmail.com>
2022-03-22 17:32:30 -0400

    add xitems to marts

--- Pokemon-XD-Tool/MainForm.Designer.cs
+++ Pokemon-XD-Tool/MainForm.Designer.cs
@@ -3045,7 +3045,6 @@ namespace Randomizer
             this.martsSellXItemsCheck.Text = "All Marts Sell X-Items";
             this.infoToolTip.SetToolTip(this.martsSellXItemsCheck, "Every mart will sell X-Items at regular price.");
             this.martsSellXItemsCheck.UseVisualStyleBackColor = true;
-            this.martsSellXItemsCheck.Visible = false;
             // 
             // randomizeMartItems
             // 
